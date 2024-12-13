a9d1004aa6a4b6ca6abb10fe20251dd441bbcfc0
王溪源 <wangxiyuan@szh1.com.cn>
2024-12-13 15:45:29 +0800

    Initial commit

---
+++ src/main/java/com/romantic/net/RomanticNetApplication.java
@@ -0,0 +1,13 @@
+package com.romantic.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanticNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanticNetApplication.class, args);
+    }
+
+}
