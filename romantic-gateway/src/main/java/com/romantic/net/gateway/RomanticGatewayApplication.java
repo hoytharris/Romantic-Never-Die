079b9901dfd519bbce8748138544401d5fb77998
王溪源 <wangxiyuan@szh1.com.cn>
2024-12-13 17:08:19 +0800

    gateway模块

---
+++ romantic-gateway/src/main/java/com/romantic/net/gateway/RomanticGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.romantic.net.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanticGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RomanticGatewayApplication.class, args);
+    }
+}
